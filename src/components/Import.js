282a0b805a243f40b81a26ed48b4c047121685e5
이재원 <jenyglee_30@naver.com>
2021-07-24 12:36:20 +0900

    edit Input.js
    인풋 컴포넌트 제작

--- src/components/Import.js
+++ src/components/Import.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Dimensions } from 'react-native'
 import styled from 'styled-components/native'
 
+const StyledInput = styled.TextInput`
+width: ${({width}) => width -40}px;
+height:60px;
+margin:3px 0;
+padding: 15px 20px;
+border-radius:10px;
+font-size:25px;
+background-color:${({theme}) => theme.itemBackground};
+color:${({theme})=>theme.main};
+`
+
+
 const Input = ()=>{
-    return
+    const width = Dimensions.get('window').width;
+    return <StyledInput width={width} />
 }
 
 export default Input;
