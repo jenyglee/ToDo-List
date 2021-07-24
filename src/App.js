330aa6333b5758a926ccdb1a1bafef758a8f0145
이재원 <jenyglee_30@naver.com>
2021-07-24 12:30:28 +0900

    create main page
    메인화면 구성

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import { StatusBar, View, Text } from "react-native";
+import React from "react";
+import styled, {ThemeProvider} from "styled-components/native";
+import {theme} from './theme'
+
+
+const Container = styled.SafeAreaView`
+    flex : 1;
+    background-color : ${({theme})=> theme.background};
+    align-items : center;
+    justify-content : flex-start;
+`
+const Title = styled.Text`
+font-size:40px;
+font-weight:600;
+color: ${({theme}) => theme.main};
+width:100%;
+align-items:flex-end;
+padding:0 20px;
+`
+
+
+export default function App() {
+    return (
+        <ThemeProvider theme={theme}>
+            <Container>
+                <StatusBar barStyle="light-content" />
+                <Title>ToDo</Title>
+            </Container>
+        </ThemeProvider>
+    );
+}
---
+++ src/icons.js
@@ -0,0 +1,13 @@
+import Check from "./../assets/icons/check.png";
+import Uncheck from "./../assets/icons/uncheck.png";
+import Delete from "./../assets/icons/delete.png";
+import Edit from "./../assets/icons/edit.png";
+
+const icon = {
+    check: Check,
+    uncheck: Uncheck,
+    delete: Delete,
+    edit: Edit,
+};
+
+export default icon;
